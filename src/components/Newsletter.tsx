b6a4f8a0edff36f5ee1cfa6a4369d9a8d6d542bf
Chan <chan@Chanui-MacBookPro.local>
2024-03-20 00:08:26 +0900

    refactor: email 입력란 수정

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -4,8 +4,8 @@ import { Button } from "./Button";
 const NewsLetter = () => {
   return (
     <form
-      action="/thank-you"
-      className="rounded-2xl border border-sky-100 p-6 dark:border-sky-700/40"
+      action="mailto:devaaronn@gmail.com"
+      className="rounded-2xl border border-sky-200 p-6 dark:border-sky-700/40"
     >
       <h2 className="flex text-sm font-semibold text-sky-900 dark:text-sky-100">
         <IoMdMailUnread className="text-xl" />
@@ -17,8 +17,8 @@ const NewsLetter = () => {
       <div className="mt-6 flex">
         <input
           type="email"
-          placeholder="이메일 주소"
-          aria-label="이메일 주소"
+          placeholder="이메일"
+          aria-label="이메일"
           required
           className="min-w-0 flex-auto appearance-none rounded-md border focus:outline-none
            border-sky-900/10 bg-white px-3 py-[clac(theme(spacing.2)-1px)] shadow-md shadow-sky-800/5 placeholder-sky-400 focus:border-sky-500"
