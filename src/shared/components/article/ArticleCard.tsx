d2ac046595d7fcb49389c7f6beb986e19d275e68
bee <bee@socar.kr>
2025-03-11 12:22:59 +0900

    feat: 아티클 컴포넌트에 태그 기능 추가

--- src/shared/components/article/ArticleCard.tsx
+++ src/shared/components/article/ArticleCard.tsx
@@ -8,6 +8,7 @@ const ArticleCard = ({ article }: { article: ArticleWithSlug }) => {
       <Card.Title href={`/posts/${article.slug}`}>{article.title}</Card.Title>
       <Card.Eyebrow>{formatDate(article.date)}</Card.Eyebrow>
       <Card.Description>{article.description}</Card.Description>
+      <Card.Tags tags={article.tags} />
       <Card.Cta>자세히 보기</Card.Cta>
     </Card>
   );
