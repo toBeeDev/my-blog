11ee291278e36f0eff0588f31a41f0e85df49f76
bee <bee.devlog@gmail.com>
2025-04-07 22:55:02 +0900

    feat: ArticlePage 컴포넌트의 검색 입력 필드 스타일 수정

--- src/components/posts/ArticlePage.tsx
+++ src/components/posts/ArticlePage.tsx
@@ -45,13 +45,13 @@ const ArticlePage = ({ articles }: { articles: ArticleWithSlug[] }) => {
       title="Dev Diary"
       intro="제가 개발하며 배우고 경험한 것들을 기록한 공간입니다. 프로젝트에서 마주한 고민, 해결 과정, 그리고 얻은 인사이트를 정리한 공간입니다."
     >
-      <div className="flex md:ml-6 justify-end max-w-[790px] mb-10 py-4">
+      <div className="flex pb-10 md:pb-20 w-full md:w-3/5">
         <input
           type="text"
           placeholder="검색어를 입력하세요"
           value={search}
           onChange={handleSearchChange}
-          className="py-2 pl-2 pr-6 border border-gray-300 rounded-md dark:bg-gray-800 dark:text-white focus:outline-none"
+          className="py-2 pl-2 w-3/5 pr-6 border border-gray-300 rounded-md dark:bg-gray-800 dark:text-white focus:outline-none"
         />
       </div>
       <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
